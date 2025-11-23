38100d5bff6c5e8f4bf41f763e53768efb957bfd
Anujram07 <anujramraika12@gmail.com>
2025-11-23 13:17:43 +0530

    sheet2

--- Contest1/Data_type.py
+++ Contest1/Data_type.py
@@ -1,6 +1,12 @@
-INT_MAX = 2_147_483_647
-LL_MAX = 9_223_372_036_854_775_807
-
-n,k,a = map(int,input().split()) 
-
-res = (n*k)/a
+n,k,a=map(int,input().split())
+result=(n*k)/a
+check=int(result)
+r1=int(-2147483648)
+r2=int(2147483647)
+if(result==check):
+    if(r1<=result<=r2):
+        print("int")
+    else:
+        print("long long")
+else:
+    print("double")
---
+++ pyhton_Sheet2/Palindrome.py
@@ -0,0 +1,10 @@
+n = input().strip()
+
+rev = n[::-1].lstrip('0')
+print(rev)
+
+if n==n[::-1]:
+    print("YES")
+else:
+    print("NO")
+
